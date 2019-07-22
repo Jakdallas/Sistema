e35c184cf4201ad04d11d507219943aa25fb06be
Carlos Sucre <csucre@unitru.edu.pe>
2019-07-22 03:07:13 -0500

    Auto-generated commit

---
+++ assets/js/carrito.js
@@ -0,0 +1,41 @@
+function getDecimals(num1) {
+
+    let num = num1,
+        match = ('' + num).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+    if (!match) {
+        return 0;
+    }
+    return Math.max(0, (match[1] ? match[1].length : 0) - (match[2] ? +match[2] : 0));
+
+}
+$(document.body).on('click', '.plus, .minus', function() {
+    var $qty = $(this).closest('.quantity').find('.qty'),
+        currentVal = parseFloat($qty.val()),
+        max = parseFloat($qty.attr('max')),
+        min = parseFloat($qty.attr('min')),
+        step = $qty.attr('step');
+
+    // Format values
+    if (!currentVal || currentVal === '' || currentVal === 'NaN') currentVal = 0;
+    if (max === '' || max === 'NaN') max = '';
+    if (min === '' || min === 'NaN') min = 0;
+    if (step === 'any' || step === '' || step === undefined || parseFloat(step) === 'NaN') step = 1;
+
+    // Change the value
+    if ($(this).is('.plus')) {
+        if (max && (currentVal >= max)) {
+            $qty.val(max);
+        } else {
+            $qty.val((currentVal + parseFloat(step)).toFixed(getDecimals(step)));
+        }
+    } else {
+        if (min && (currentVal <= min)) {
+            $qty.val(min);
+        } else if (currentVal > 0) {
+            $qty.val((currentVal - parseFloat(step)).toFixed(getDecimals(step)));
+        }
+    }
+
+    // Trigger change event
+    $qty.trigger('change');
+});
